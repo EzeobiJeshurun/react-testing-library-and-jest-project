d576b42b0b56e672d0c904fd71e8804a3c3540a4
jeshurun ezeobi <jeshurunezeobi@jeshuruns-MacBook-Pro.local>
2021-11-10 17:15:36 -0800

    add test for color change button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
-import Button from './components/Button/Button';
+import { useState } from "react";
+import "./App.css";
+import Button from "./components/Button/Button";
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
   return (
     <div>
-      <Button style={{backgroundColor: "red"}}>
-       Change to blue
+      <Button 
+         style={{ backgroundColor: buttonColor }}
+         onClick={()=> setButtonColor(newButtonColor)}
+        >
+        Change to {newButtonColor}
       </Button>
-    </div>  
+    </div>
   );
 }
 
